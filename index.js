3e7280bf1cd89a63206bc8deb274ffc29f935b9c
gamorax80 <163062456+gamorax80@users.noreply.github.com>
2024-03-12 17:27:16 +0700

    index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+const count = require('text-count');
+
+function gamoraaaaa() {
+  const text = "G A M O R A A A A A"
+  return {
+    text,
+    length: count.charCount(text)
+  }
+}
+
+
+module.exports = Gamoraaaaa
